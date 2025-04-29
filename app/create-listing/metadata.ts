d2d825f7ec90bb56a9acd0883df1729b9bfa0f38
Tai-brother <138606172+Tai-brother@users.noreply.github.com>
2025-04-29 23:49:15 +0700

    fix 6

--- app/create-listing/metadata.ts
+++
@@ -1,6 +0,0 @@
-import { Metadata } from "next"
-
-export const metadata: Metadata = {
-  title: 'Đăng tin cho thuê - Rentify',
-  description: 'Đăng tin cho thuê phòng trọ, căn hộ trên Rentify',
-} 
